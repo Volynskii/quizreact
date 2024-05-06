da30e56f71eac1dfe52d27cb0f86ddbea978613d
Anton Wolynski <50911673+Volynskii@users.noreply.github.com>
2024-05-06 18:14:27 +0300

    commit;

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -10,12 +10,33 @@ function QuizApp() {
     const [selectedOption, setSelectedOption] = useState('');
     const [score, setScore] = useState(0);
     const [questionsLength, setQuestionsLength] = useState(0);
+    const [answers,setAnswers] = useState();
+
+    const quizId = window.location.pathname.split("/")[1];
+    const action = 'answer'
+
+    const [questionId, setQuestionId] = useState('');
+    const [userId,setUserId] = useState('');
+    console.log('quizId', quizId )
+    console.log('questionId',questionId)
+    console.log('userId',userId)
+
+    console.log('action', action)
+    console.log('answers[]', selectedOption)
 
     const fetchData = async () => {
         try {
-            const url = 'https://quiz.vgtrk.com/index.php?action=data&id=63';
+            const url = `https://quiz.vgtrk.com/index.php?action=data&id=${quizId}`;
             const response = await axios.get(url);
+            // console.log('data!', )
             const quizData = response.data.data.quiz_q;
+            const quest_id = response.data.data.quiz_q[currentQuestion].id;
+
+            const uid = await fetchCookie();
+            setUserId(uid);
+            setQuestionId(quest_id);
+
+
             setQuizData(quizData);
             setQuestionsLength(quizData.length);
             setIsLoaded(true);
@@ -31,14 +52,8 @@ function QuizApp() {
             const response = await axios.post(url);
             const quiz_uid = response.data.data.uid;
             // Устанавливаем куку
-            Cookies.set('quiz_uid', quiz_uid);
+            return quiz_uid;
 
-            // Получаем куку
-            const cookieValue = Cookies.get('myCookie');
-            console.log(cookieValue); // Выведет 'cookieValue'
-
-            // Удаляем куку
-            Cookies.remove('myCookie');
         } catch (error) {
             console.error('Error fetching data:', error);
             setIsLoaded(false);
@@ -46,14 +61,37 @@ function QuizApp() {
     }
 
     useEffect(() => {
+        // fetchCookie();
         fetchData();
-        fetchCookie();
     }, []);
 
     const progressBarWidth = ((currentQuestion + 1) / questionsLength) * 100;
 
-    const handleOptionSelect = (option) => {
-        setSelectedOption(option);
+    const handleOptionSelect = async (option) => {
+
+       await submitAnswer(option);
+    };
+
+    const submitAnswer = async (option) => {
+        try {
+            const url = 'https://quiz.vgtrk.com';
+            const response = await axios.post(url, {
+                "quiz_id": quizId,
+                "quest_id": questionId,
+                "uid": userId,
+                "answers[]": option,
+                "action": "answer"
+            }, {
+                headers: {
+                    'Content-Type': 'multipart/form-data' // Set content type to multipart/form-data
+                }
+            });
+
+            console.log('response!', response.data.data);
+
+        } catch (error) {
+            console.error('Error submitting answer:', error);
+        }
     };
 
     const handleNextQuestion = () => {
@@ -78,7 +116,7 @@ function QuizApp() {
                             <ul className="question-answers">
                                 {quizData[currentQuestion]?.quiz_a.map(({ title, id }) => (
                                     <li className="question-answer" key={id}>
-                                        <input type="radio" id={id} name="answer" value={title} onChange={() => handleOptionSelect(title)} checked={selectedOption === title} />
+                                        <input type="radio" id={id} name="answer" value={title} onChange={() => handleOptionSelect(id)} checked={selectedOption === title} />
                                         <label htmlFor={id} className="answer-container-text">
                                             <i className="control"></i>
                                             <span className="answer text">{title}</span>
